42c4e633453db9fa68d69318699f9e2efe7c451e
Sebastian Nieroda <sebastian.nieroda@codeq.pl>
2022-12-09 20:07:48 +0100

    Initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    // Home page
+    public function homePage(){
+        return 'To jest strona główna';
+    }
+}
